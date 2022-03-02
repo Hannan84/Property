ed5b76a3c85a5a314e1c535fb3337daecfdf303d
Hannan Sarkar <hannansarkar84@gmail.com>
2022-03-02 23:35:49 +0600

    create and modified some file

--- database/migrations/2022_03_02_131202_create_media_table.php
+++ database/migrations/2022_03_02_131202_create_media_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('properties', function (Blueprint $table) {
+        Schema::create('media', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('featured_image');
-            $table->unsignedBigInteger('price');
-            $table->unsignedBigInteger('sale')->default(1)->comment('0=rent,1=sale');
-            $table->unsignedBigInteger('type')->default(1)->comment('0=land,1=apartment,2=villa');
-            $table->string('bedrooms')->nullable();
+            $table->unsignedBigInteger('property_id')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('properties');
+        Schema::dropIfExists('media');
     }
 };
